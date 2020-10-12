b29f564e71dc7456cab8b5b0dffcde41159e9c8e
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-12 17:57:49 +0300

    create a test for instance

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
-# Create your tests here.
+
+class ImageTestCase(TestCase):
+
+    def setUp(self):
+        self.new_image = Image(
+            image_description='Test description', image_name='Family', image='image/path')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image, Image))
