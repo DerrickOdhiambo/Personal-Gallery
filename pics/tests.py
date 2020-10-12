cc92f427d8aaad9f606e7f0c29b689c6c6ed9147
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-12 18:06:21 +0300

    create a test for save_image method

--- pics/tests.py
+++ pics/tests.py
@@ -10,3 +10,8 @@ class ImageTestCase(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_image, Image))
+
+    def test_save_image(self):
+        self.new_image.save_image()
+        self.new_image = Image.objects.all()
+        self.assertTrue(len(self.new_image) > 0)
