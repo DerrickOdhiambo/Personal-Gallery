8f2eb15eab64a8c978412cbf3651335f7b4cc5f5
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-12 18:23:43 +0300

    create a test for delete method

--- pics/tests.py
+++ pics/tests.py
@@ -15,3 +15,13 @@ class ImageTestCase(TestCase):
         self.new_image.save_image()
         self.new_image = Image.objects.all()
         self.assertTrue(len(self.new_image) > 0)
+
+    def test_get_image_by_id(self):
+        self.new_image.save_image()
+        self.new_image = Image.get_image_by_id(1)
+        self.assertTrue(isinstance(self.new_image, Image))
+
+    def test_delete_image(self):
+        Image.delete_image(self.new_image.id)
+        images = Image.all_images()
+        self.assertTrue(len(images) == 0)
