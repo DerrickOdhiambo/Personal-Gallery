3a62417386a5f61c95a8c5d05b58365ca51ee5ee
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-12 21:21:10 +0300

    create a test for the delete method in the Image model

--- pics/tests.py
+++ pics/tests.py
@@ -5,8 +5,16 @@ from .models import Image, Category, Location
 class ImageTestCase(TestCase):
 
     def setUp(self):
+        # new location
+        self.new_location = Location(location_name='Kenya')
+        self.new_location.save_location()
+
+        # Creating a new category and saving it
+        self.new_category = Category(category_name='Travel')
+        self.new_category.save_category()
+
         self.new_image = Image(
-            image_description='Test description', image_name='Family', image='image/path')
+            image_description='Test description', image_name='Family', image='image/path', location=self.new_location)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_image, Image))
