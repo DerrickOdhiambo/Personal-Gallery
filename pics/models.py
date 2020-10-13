230bec628595dcb9a053f9db96ca230ef14b37ae
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-13 07:52:59 +0300

    add a filter name to the category search method

--- pics/models.py
+++ pics/models.py
@@ -58,10 +58,10 @@ class Image(models.Model):
 
     @classmethod
     def search_image_by_category(cls, image_category):
-        images = cls.objects.filter(category__name__icontains=image_category)
+        images = cls.objects.filter(image_name__icontains=image_category)
         return images
 
     @classmethod
     def filter_by_location(cls, location_id):
-        images = cls.objects.filter(location__id=location_id)
+        images = cls.objects.filter(location_id=location_id)
         return images
