eddbe48b9ff99e83db984e27572f78c7e15b75d5
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-13 07:26:32 +0300

    add a function view to search by category

--- pics/views.py
+++ pics/views.py
@@ -5,3 +5,20 @@ from .models import Image
 def index(request):
     display_images = Image.all_images
     return render(request, 'index.html', {'display_images': display_images})
+
+
+def search_by_category(request):
+    if 'q' in request.GET and request.GET["q"]:
+        search_term = request.GET.get("q")
+        searched_images = Image.search_image_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {'searched_images': searched_images, 'message': message})
+
+
+def filter_by_location(request, location_id):
+    search_term = '1'
+    searched_images = Image.filter_by_location(location_id)
+    message = f"{search_term}"
+
+    return render(request, 'search.html', {'searched_images': searched_images, 'message': message})
