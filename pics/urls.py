1326ebdfebdb126331db71b4308bd394189f54c7
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-13 07:54:46 +0300

    add url path for search and filter views

--- pics/urls.py
+++ pics/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('search/', views.search_by_category, name='search'),
+    path('location/', views.filter_by_location, name='location'),
 ]
 
 if settings.DEBUG:
